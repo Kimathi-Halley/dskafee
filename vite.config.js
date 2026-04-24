380d600241e86cfc0ca42bd35d69a4f338685836
Kimathi-Halley <kimathi.a.halley@gmail.com>
2026-04-24 17:53:19 -0400

    removed coming soon page

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        home: resolve(__dirname, "home/index.html"),
         menu: resolve(__dirname, "menu/index.html"),
         about: resolve(__dirname, "about/index.html"),
-        project: resolve(__dirname, "project/index.html"),
         contact: resolve(__dirname, "contact/index.html"),
       },
     },
